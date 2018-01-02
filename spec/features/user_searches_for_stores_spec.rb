51ddd3af58614b3b60dbe72fa9e633d39c105749
Hugh Morrison <hmorri32@gmail.com>
2018-01-02 10:25:13 -0700

    specs out path change when click on search

--- spec/features/user_searches_for_stores_spec.rb
+++ spec/features/user_searches_for_stores_spec.rb
@@ -1,9 +1,15 @@
 require "rails_helper"
 
 RSpec.feature "User uses the search bar" do
-  before { create_list(:item, 3); visit "/" }
-  scenario "fills it in with 80202. clicks search" do
+  before { create_list(:item, 10); visit "/"; fill_in 'zip_code', with: 80202; click_on "Search" }
 
-    save_and_open_page
+  describe "fills it in with 80202. clicks search" do
+    it "my path should be /search" do
+      expect(current_path).to eq('/search')
+    end
+
+    it "I should see a list of stores within 25 miles of 80202" do
+      expect(page).to have_content("All stores within 25 miles of 80202")
+    end
   end
 end
