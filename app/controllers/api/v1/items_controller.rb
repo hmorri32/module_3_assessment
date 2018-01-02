53e789d6cc8d3d741c2449b9703c00d6925ce459
Hugh Morrison <hmorri32@gmail.com>
2018-01-02 09:28:57 -0700

    api items controller show method and set_item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::ItemsController < Api::V1::BaseController
+  before_action :set_item, only: [:show]
+
   def index
     json_response Item.all
+  end
+
+  def show
+    json_response @item
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
