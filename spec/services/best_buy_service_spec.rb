46d86818efe206d709bad6176bd4dcb9be709252
Hugh Morrison <hmorri32@gmail.com>
2018-01-02 11:17:38 -0700

    spec best buy service

--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -10,5 +10,11 @@ RSpec.describe BestBuyService do
   end
 
   describe "instance methods" do
+    describe ".stores_within_25_miles" do
+      it "should return all stores within a 25 mile radius of zip code" do
+        stuff = best_buy.stores_within_25_miles
+        expect(stuff.length).to eq(17)
+      end
+    end
   end
 end
