cd050d3484dd095049232409bc12139b7b5905b4
Hugh Morrison <hmorri32@gmail.com>
2018-01-02 09:38:58 -0700

    destroy method on api items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ItemsController < Api::V1::BaseController
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :destroy]
 
   def index
     json_response Item.all
@@ -7,6 +7,11 @@ class Api::V1::ItemsController < Api::V1::BaseController
 
   def show
     json_response @item
+  end
+
+  def destroy
+    @item.destroy
+    head :no_content
   end
 
   private
