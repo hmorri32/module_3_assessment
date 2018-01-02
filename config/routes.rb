b8bbc853d562216015082a8c47c91ec6dc0ead42
Hugh Morrison <hmorri32@gmail.com>
2018-01-02 09:20:57 -0700

    adds namespaced items api route index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root 'items#index'
 
-  resources :items, only: [:index, :show]
+  resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
-  resources :users, only: [:index, :show]
+  resources :users,  only: [:index, :show]
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :items, only: [:index]
+    end
+  end
 end
