86f54f515a3af3ed80499cd86a54e6c5b4451e78
Hugh Morrison <hmorri32@gmail.com>
2018-01-02 11:37:46 -0700

    VCR user search spec

--- spec/features/user_searches_for_stores_spec.rb
+++ spec/features/user_searches_for_stores_spec.rb
@@ -1,34 +1,44 @@
 require "rails_helper"
 
 RSpec.feature "User uses the search bar" do
-  before { create_list(:item, 10); visit "/"; fill_in 'zip_code', with: 80202; click_on "Search" }
+  before { create_list(:item, 10); visit "/"; fill_in 'zip_code', with: 80202; }
 
   describe "fills it in with 80202. clicks search" do
     it "my path should be /search" do
-      expect(current_path).to eq('/search')
+      VCR.use_cassette('user_searches/path_changes', :match_requests_on => [:method]) do
+        click_on "Search"
+        expect(current_path).to eq('/search')
+      end
     end
 
     it "I should see a list of stores within 25 miles of 80202, paginated" do
-      expect(page).to have_content("All stores within 25 miles of 80202")
-      expect(page).to have_content("17 Total Stores")
-      expect(page).to have_content("Cherry Creek Shopping Center")
-      expect(page).to have_content("Belmar")
-      expect(page).to have_content("Colorado Blvd")
-      expect(page).to have_selector('.store-name', count: 10)
-      expect(page).to have_selector('.next_page')
+      VCR.use_cassette('user_searches/sees_stores', :match_requests_on => [:method]) do
+        click_on "Search"
+        expect(page).to have_content("All stores within 25 miles of 80202")
+        expect(page).to have_content("17 Total Stores")
+        expect(page).to have_content("Cherry Creek Shopping Center")
+        expect(page).to have_content("Belmar")
+        expect(page).to have_content("Colorado Blvd")
+        expect(page).to have_selector('.store-name', count: 10)
+        expect(page).to have_selector('.next_page')
+      end
     end
 
     it "should paginate results and be navigable" do
-      expect(page).to have_content("17 Total Stores")
-      expect(page).to have_selector('.store-name', count: 10)
-      expect(page).to have_selector('.next_page')
-      expect(page).to have_selector('.previous_page.disabled')
+      VCR.use_cassette('user_searches/sees_pagination', :match_requests_on => [:method]) do
+        click_on 'Search'
 
-      click_on 'Next'
+        expect(page).to have_content("17 Total Stores")
+        expect(page).to have_selector('.store-name', count: 10)
+        expect(page).to have_selector('.next_page')
+        expect(page).to have_selector('.previous_page.disabled')
 
-      expect(page).to have_selector('.store-name', count: 7)
-      expect(page).to have_selector('.next_page.disabled')
-      expect(page).to have_current_path("/search?commit=Search&page=2&utf8=✓&zip_code=80202")
+        click_on 'Next'
+
+        expect(page).to have_selector('.store-name', count: 7)
+        expect(page).to have_selector('.next_page.disabled')
+        expect(page).to have_current_path("/search?commit=Search&page=2&utf8=✓&zip_code=80202")
+      end
     end
   end
 end
