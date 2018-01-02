91451cb81d3a024cfe5a85416843e841ccd47d04
Hugh Morrison <hmorri32@gmail.com>
2018-01-02 09:29:16 -0700

    specs out happy and sap paths for get api/v1/items/:id

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "ITEMS API", type: :request do
-  let!(:items) { create_list(:item, 100) }
+  let!(:items)  { create_list(:item, 100) }
+  let(:item_id) { items.first.id }
 
   describe "GET /api/v1/items" do
     before { get '/api/v1/items' }
@@ -13,6 +14,33 @@ RSpec.describe "ITEMS API", type: :request do
 
     it "returns HTTP status code 200" do
       expect(response).to have_http_status 200
+    end
+  end
+
+  describe "GET /api/v1/items/:id" do
+    before { get "/api/v1/items/#{item_id}" }
+    context "for a valid resource" do
+      it "returns an item by item" do
+        expect(json).to_not be_empty
+        expect(json['id']).to eq(1)
+        expect(json['name']).to eq(items.first.name)
+      end
+
+      it "returns HTTP status code 200" do
+        expect(response).to have_http_status 200
+      end
+    end
+
+    context "for an invalid resource" do
+      let(:item_id) { 0 }
+
+      it "returns an error message" do
+        expect(response.body).to match /Couldn't find Item/
+      end
+
+      it "returns status code 404" do
+        expect(response).to have_http_status(404)
+      end
     end
   end
 end
