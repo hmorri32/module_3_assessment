56d0daeeb82d78d16ed4fc35eb076cd7f38d0019
Hugh Morrison <hmorri32@gmail.com>
2018-01-02 11:16:15 -0700

    @stores in index method of search controller, paginated

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
-
+    @bb     = BestBuyService.new(params[:zip_code])
+    @total  = @bb.stores_within_25_miles.map { |store| BestBuyStore.new(store) }
+    @stores = @total.paginate(:page => params[:page], :per_page => 10)
   end
 end
