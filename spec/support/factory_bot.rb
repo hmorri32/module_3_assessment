176b5c59084083b47537632df03a8618af331dd7
Hugh Morrison <hmorri32@gmail.com>
2018-01-02 07:04:08 -0700

    adds db cleaner to factory girl

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,4 +1,15 @@
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-end
 
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+
+    begin
+      DatabaseCleaner.start
+      FactoryBot.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
+end
