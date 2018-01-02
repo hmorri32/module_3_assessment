c3a142bfe7f5e0a1de1bd2874de951b4e83e3388
Hugh Morrison <hmorri32@gmail.com>
2018-01-02 11:16:51 -0700

    initialize best buy service

--- app/servies/best_buy_service.rb
+++ app/servies/best_buy_service.rb
@@ -1,3 +1,33 @@
 class BestBuyService
-  
+  def initialize(zip_code=80202)
+    @zip_code = zip_code
+    @client   = configure_faraday
+  end
+
+  def configure_faraday
+    Faraday.new("https://api.bestbuy.com/v1/") do |f|
+      f.adapter Faraday.default_adapter
+      f.params[:apiKey] = ENV['YUNG_BB_KEY']
+    end
+  end
+
+  def stores_within_25_miles
+    get_json("stores(area(80202,25))?format=json&pageSize=100&show=longName,city,distance,storeType,phone")[:stores]
+  end
+
+  private
+  attr_reader :client, :zip_code
+
+  def get_json(url)
+    response = client.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
+
+
+# curl "https://api.bestbuy.com/v1/stores(area(55423))?format=json&show=storeType,longName,city,distance,phone&apiKey=YourAPIKey"
+# curl "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&distance=25&show=phone,storeType,longName,city,distance&apiKey=qy8vdpfcgsxtfw657mnbu7nc"
+
+# curl "https://api.bestbuy.com/v1/stores(area(55423,25))?format=json&show=longName,city,distance,storeType&apiKey=YourAPIKey"
+
+# curl "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=longName,city,distance,storeType,phone&apiKey=qy8vdpfcgsxtfw657mnbu7nc"
