55ad81b155cf394ff129ce0c95634775c6f3ea0f
Hugh Morrison <hmorri32@gmail.com>
2018-01-02 09:46:09 -0700

    create method, strong params

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,6 +9,11 @@ class Api::V1::ItemsController < Api::V1::BaseController
     json_response @item
   end
 
+  def create
+    @item = Item.create!(item_params)
+    json_response @item, :created
+  end
+
   def destroy
     @item.destroy
     head :no_content
@@ -18,5 +23,9 @@ class Api::V1::ItemsController < Api::V1::BaseController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :image_url)
   end
 end
