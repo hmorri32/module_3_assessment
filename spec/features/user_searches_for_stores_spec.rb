7bdb12a40cca826ca117c81a32005a4ba6fe993f
Hugh Morrison <hmorri32@gmail.com>
2018-01-02 11:17:26 -0700

    user searches for store spec, paginated

--- spec/features/user_searches_for_stores_spec.rb
+++ spec/features/user_searches_for_stores_spec.rb
@@ -8,8 +8,27 @@ RSpec.feature "User uses the search bar" do
       expect(current_path).to eq('/search')
     end
 
-    it "I should see a list of stores within 25 miles of 80202" do
+    it "I should see a list of stores within 25 miles of 80202, paginated" do
       expect(page).to have_content("All stores within 25 miles of 80202")
+      expect(page).to have_content("17 Total Stores")
+      expect(page).to have_content("Cherry Creek Shopping Center")
+      expect(page).to have_content("Belmar")
+      expect(page).to have_content("Colorado Blvd")
+      expect(page).to have_selector('.store-name', count: 10)
+      expect(page).to have_selector('.next_page')
+    end
+
+    it "should paginate results and be navigable" do
+      expect(page).to have_content("17 Total Stores")
+      expect(page).to have_selector('.store-name', count: 10)
+      expect(page).to have_selector('.next_page')
+      expect(page).to have_selector('.previous_page.disabled')
+
+      click_on 'Next'
+
+      expect(page).to have_selector('.store-name', count: 7)
+      expect(page).to have_selector('.next_page.disabled')
+      expect(page).to have_current_path("/search?commit=Search&page=2&utf8=✓&zip_code=80202")
     end
   end
 end
