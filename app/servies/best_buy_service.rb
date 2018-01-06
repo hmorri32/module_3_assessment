25fb12aba14c755dae187b5753d0cd78afec8d9f
Hugh Morrison <hmorri32@gmail.com>
2018-01-05 17:24:53 -0700

    refactor wide method in bb service

--- app/servies/best_buy_service.rb
+++ app/servies/best_buy_service.rb
@@ -12,14 +12,22 @@ class BestBuyService
   end
 
   def stores_within_25_miles
-    get_json("stores(area(#{@zip_code},25))?format=json&pageSize=100&show=longName,city,distance,storeType,phone")[:stores]
+    get_json("stores(area(#{@zip_code},25))", filter)[:stores]
   end
 
   private
     attr_reader :client, :zip_code
 
-    def get_json(url)
+    def get_json(url, params)
       response = client.get(url)
       JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def filter
+      {
+        format: 'json',
+        show: 'longName, city, distance, storeType, phone',
+        pageSize: 100,
+      }
     end
 end
